e8e77313da964cd36ee0e57154daadcea79e85fb
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-02-18 23:49:15 +0600

    Done for today

--- database/migrations/2024_02_18_160235_create_animals_table.php
+++ database/migrations/2024_02_18_160235_create_animals_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('animals', function (Blueprint $table) {
             $table->id();
+            $table->date("updated_date");
+            $table->string("image_url");
+            $table->string("name");
+            $table->string("sci_name")->nullable();
+            $table->text("animal_fact")->nullable(); // Describe the reasone why this animal becoming extincts
+            $table->string("population_status"); // 1. Endangered, 2. Critically Endangered, 3. Vulnerable, 4. Near Threatened, 5. Least Concern
+            $table->text("habitat"); // Brief about where they live
+            $table->text("conservation_efforts")->default("Unknown"); // Description of ongoing conservation efforts or initiatives for the species.
+            $table->text("threat_factors")->default("Unknown"); // Describe the reasone why this animal becoming extincts
             $table->timestamps();
         });
     }
