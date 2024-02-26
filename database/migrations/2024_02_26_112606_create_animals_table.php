4d3283d90300ccd71914b81c579e44c8aea2a43b
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-02-26 18:01:11 +0600

    Saving the current progress

--- database/migrations/2024_02_26_112606_create_animals_table.php
+++ database/migrations/2024_02_26_112606_create_animals_table.php
@@ -6,24 +6,24 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('animals', function (Blueprint $table) {
             $table->id();
+            $table->date("updated_date");
+            $table->string("image_url");
+            $table->string("name");
+            $table->string("sci_name");
+            $table->text("animal_fact");
+            $table->string("population_status");
+            $table->integer("population_count_est");
+            $table->text("habitat")->nullable();
+            $table->text("conservation_efforts")->nullable();
+            $table->text("threat_factors")->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('animals');
