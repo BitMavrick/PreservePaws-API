1463495703604eb784b9b2c4b0109d6c6a7549ba
Mehedi Hasan <email.mehedi.bd@gmail.com>
2024-02-28 20:56:48 +0600

    Saving the progress

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Http\Controllers\Api\V1\AnimalController;
-use App\Http\Controllers\Api\V1\CategoryeController;
-use App\Http\Controllers\Api\V1\TypeController;
+// use App\Http\Controllers\Api\V1\CategoryeController;
+// use App\Http\Controllers\Api\V1\TypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+// * Defaults route implemented already -------------------------
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
