62f1e55085b06ec1e6da428433693bb9aae994c4
BitMavrick <email.mehedi.bd@gmail.com>
2024-03-04 13:00:25 +0600

    adding new routes

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Web\WebController;
 
 
+/*
 Route::get('/', function () {
     return view('index');
 });
+*/
+
+Route::get('/', [WebController::class, 'index'])->name('index');
+
