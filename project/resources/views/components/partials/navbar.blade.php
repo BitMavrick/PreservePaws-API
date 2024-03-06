9d47e9d0b2f354f9b4a5312dc980bbab692ac856
BitMavrick <email.mehedi.bd@gmail.com>
2024-03-06 21:49:36 +0600

    Blade file breking into components

---
+++ project/resources/views/components/partials/navbar.blade.php
@@ -0,0 +1,35 @@
+<div class="site-mobile-menu site-navbar-target">
+		<div class="site-mobile-menu-header">
+			<div class="site-mobile-menu-close">
+				<span class="icofont-close js-menu-toggle"></span>
+			</div>
+		</div>
+		<div class="site-mobile-menu-body"></div>
+	</div>
+
+	<nav class="site-nav">
+		<div class="container">
+			<div class="menu-bg-wrap">
+				<div class="site-navigation">
+					<div class="row g-0 align-items-center">
+						<div class="col-6">
+							<a href="index.html" class="logo m-0 float-start">PreservePaws</a>
+						</div>
+
+						<div class="col-6 text-end">
+							<a href="#" class="burger ms-auto float-end site-menu-toggle js-menu-toggle d-inline-block d-lg-none light">
+								<span></span>
+							</a>
+
+							<ul class="js-clone-nav d-none d-lg-inline-block text-end site-menu mx-auto">
+								<li class="active"><a href="index.html">Home</a></li>
+								<li><a href="category.html">API</a></li>
+								<li><a href="category.html">About</a></li>
+								<li><a href="category.html">Policies</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</nav>
